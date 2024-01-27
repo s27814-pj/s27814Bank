f70bfb254f6d29c06ae47a4ec923e0f230e47bab
s27814-pj <s27814@pjwstk.edu.pl>
2024-01-27 12:07:19 +0100

    1st commit

---
+++ src/main/java/com/example/demo/S27814BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27814BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S27814BankApplication.class, args);
+	}
+
+}
